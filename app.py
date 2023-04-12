d4b7df33480c238785bcbd74f1a615e8edb19cf3
mkmenta <mkmenta95@gmail.com>
2023-04-12 16:44:23 +0200

    Allow markdown

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from flask import Flask, redirect, render_template, request
 from flask_session import Session
 from flask_login import login_required, current_user
+from markdown import markdown
 
 from mongoengine import connect
 from models.chat import Chat
@@ -117,6 +118,9 @@ def send_message(chat_id):
 @app.template_filter('env_override')
 def env_override(value, key):
     return os.getenv(key, value)
+
+
+app.jinja_env.globals.update(markdown=markdown)
 
 
 """ Tackle plan:
