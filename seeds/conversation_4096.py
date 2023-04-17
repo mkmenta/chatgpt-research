8b03dcb4f224c17f1e2abd8b1a1e9e2ad281c487
mkmenta <mkmenta95@gmail.com>
2023-04-17 21:33:57 +0200

    Make script to test 4096

--- seeds/conversation_4096.py
+++ seeds/conversation_4096.py
@@ -9,6 +9,9 @@ from models.message import Message
 from models.user import User
 from utils import now_mytz
 
+TEXT = "ChatGPT is an AI-powered conversational chatbot created using Natural Language Processing (NLP) and Machine Learning (ML) technologies. The development of ChatGPT involved several steps, including: 1. Data Collection: Massive amounts of conversational data were collected from various sources, including social media, messaging apps, and online forums. 2. Preprocessing: The collected data was preprocessed to remove irrelevant information, such as emoji, URLs, and special characters. 3. Training: The preprocessed data was then used to train a deep learning model based on the GPT (Generative Pre-trained Transformer) architecture. The model was trained to understand natural language and generate responses based on the context of the conversation. 4. Testing and Fine-tuning: After training, the model was tested on a set of validation data to ensure its performance. The model was then fine-tuned based on the feedback received from the validation data. ChatGPT is an AI-powered conversational chatbot created using Natural Language Processing (NLP) and Machine Learning (ML) technologies. The development of ChatGPT involved several steps, including: 1. Data Collection: Massive amounts of conversational data were collected from various sources, including social media, messaging apps, and online forums. 2. Preprocessing: The collected data was preprocessed to remove irrelevant information, such as emoji, URLs, and special characters. 3. Training: The preprocessed data was then used to train a deep learning model based on the GPT (Generative Pre-trained Transformer) architecture. The model was trained to understand natural language and generate responses based on the context of the conversation. 4. Testing and Fine-tuning: After training, the model was tested on a set of validation data to ensure its performance. The model was then fine-tuned based on the feedback received from the validation data."
+NTOKENS = 392
+
 if __name__ == "__main__":
     connect(host=os.environ.get('MONGOURI'))
 
@@ -17,14 +20,13 @@ if __name__ == "__main__":
     chat = Chat(title=now_mytz().strftime("%d/%m/4096, %H:%M"), user=user)
     chat.save()
 
-    n_tokens = 400
-    for i in range((4096 // n_tokens)+1):
-        last_usr_msg = Message(role="user", content="Message 1")
-        last_usr_msg.num_tokens = n_tokens
+    for i in range((4096 // (NTOKENS*2))+1):
+        last_usr_msg = Message(role="user", content=TEXT)
+        last_usr_msg.num_tokens = NTOKENS
         last_usr_msg.save()
         chat.messages.append(last_usr_msg)
-        last_bot_msg = Message(role="assistant", content="Answer 1")
-        last_bot_msg.num_tokens = n_tokens
+        last_bot_msg = Message(role="assistant", content=TEXT)
+        last_bot_msg.num_tokens = NTOKENS
         last_bot_msg.save()
         chat.messages.append(last_bot_msg)
     chat.save()
