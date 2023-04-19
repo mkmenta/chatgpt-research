4d439b43573c5b1498d64fa667316bb0b4078cac
mkmenta <mkmenta95@gmail.com>
2023-04-19 22:27:12 +0200

    Register page working

--- routes/users.py
+++ routes/users.py
@@ -19,21 +19,20 @@ def load_user(user_id):
     return User.objects.get(id=ObjectId(user_id))
 
 
-# @blueprint.route('/register', methods=['GET'])
-# def register_user():
-#     return render_template('users/register.html')
+@blueprint.route('/register', methods=['GET'])
+def register_user():
+    return render_template('users/register.html')
 
 
-# @blueprint.route('/register', methods=['POST'])
-# def post_register_user():
-#     try:
-#         user = User.register(**request.form)
-#     except NotUniqueError:
-#         flash("User already exists.", "error")
-#         return redirect("/register")
-#     login_user(user)
-#     flash('Welcome!', 'success')
-#     return redirect('/')
+@blueprint.route('/register', methods=['POST'])
+def post_register_user():
+    try:
+        user = User.register(**request.form)
+    except NotUniqueError:
+        flash("User already exists.", "error")
+        return redirect("/register")
+    flash('User registered!', 'success')
+    return redirect("/register")
 
 
 @blueprint.route('/login', methods=['GET'])
